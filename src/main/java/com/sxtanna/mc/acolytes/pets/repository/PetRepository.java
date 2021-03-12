756a923926955b22bf6b5fcfaf6e361d1c4087e9
Sxtanna <ranald.taylor@gmail.com>
2021-03-12 05:54:26 -0500

    updated repository with accessors

--- src/main/java/com/sxtanna/mc/acolytes/pets/repository/PetRepository.java
+++ src/main/java/com/sxtanna/mc/acolytes/pets/repository/PetRepository.java
@@ -1,6 +1,13 @@
 package com.sxtanna.mc.acolytes.pets.repository;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.sxtanna.mc.acolytes.base.State;
+import com.sxtanna.mc.acolytes.data.Pet;
+
+import java.util.Collection;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public interface PetRepository extends State
 {
@@ -10,5 +17,12 @@ public interface PetRepository extends State
 
 	@Override
 	void kill();
+
+
+	@NotNull CompletableFuture<Collection<Pet>> select(@NotNull final UUID uuid);
+
+	@NotNull CompletableFuture<Void> delete(@NotNull final UUID uuid);
+
+	@NotNull CompletableFuture<Void> insert(@NotNull final UUID uuid, @NotNull final Collection<Pet> pets);
 
 }
--- src/main/java/com/sxtanna/mc/acolytes/pets/repository/PetRepositoryLocal.java
+++ src/main/java/com/sxtanna/mc/acolytes/pets/repository/PetRepositoryLocal.java
@@ -1,32 +1,70 @@
 package com.sxtanna.mc.acolytes.pets.repository;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import com.sxtanna.mc.acolytes.AcolytesPlugin;
+import com.sxtanna.mc.acolytes.data.Pet;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import static com.google.common.base.Preconditions.checkState;
 
 public final class PetRepositoryLocal implements PetRepository
 {
 
 	@NotNull
 	private final AcolytesPlugin plugin;
+	@NotNull
+	private final File           folder;
 
 
 	public PetRepositoryLocal(@NotNull final AcolytesPlugin plugin)
 	{
 		this.plugin = plugin;
+		this.folder = new File(plugin.getDataFolder(), "pet-repository");
 	}
 
 
 	@Override
 	public void load()
 	{
-
+		checkState(this.folder.exists() || this.folder.mkdirs(), "could not create pet-repository folder");
 	}
 
 	@Override
 	public void kill()
 	{
 
+	}
+
+
+	@Contract("_ -> new")
+	private @NotNull File file(@NotNull final UUID uuid)
+	{
+		return new File(this.folder, String.format("%s.json", uuid));
+	}
+
+
+	@Override
+	public @NotNull CompletableFuture<Collection<Pet>> select(@NotNull final UUID uuid)
+	{
+		return CompletableFuture.completedFuture(null);
+	}
+
+	@Override
+	public @NotNull CompletableFuture<Void> delete(@NotNull final UUID uuid)
+	{
+		return CompletableFuture.completedFuture(null);
+	}
+
+	@Override
+	public @NotNull CompletableFuture<Void> insert(@NotNull final UUID uuid, @NotNull final Collection<Pet> pets)
+	{
+		return CompletableFuture.completedFuture(null);
 	}
 
 }

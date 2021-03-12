e90dd4c755b596c784b60e298fbdfd4c4bec4262
Sxtanna <ranald.taylor@gmail.com>
2021-03-12 13:03:52 -0500

    updated pet interface to accept backend pet entity

--- src/main/java/com/sxtanna/mc/acolytes/data/Pet.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/Pet.java
@@ -3,8 +3,15 @@ package com.sxtanna.mc.acolytes.data;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import com.sxtanna.mc.acolytes.backend.PetEntity;
+
 public interface Pet
 {
+
+	@Nullable PetEntity getEntity();
+
+	void setEntity(@Nullable PetEntity entity);
+
 
 	@Nullable <T> T select(@NotNull final PetAttribute<T> attribute);
 

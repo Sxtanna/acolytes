0679e198765a5a95e75ada0832688a3ab7b50bc6
Sxtanna <ranald.taylor@gmail.com>
2021-03-13 13:30:38 -0500

    updated pet with default method to create head item from skin attribute

--- src/main/java/com/sxtanna/mc/acolytes/data/Pet.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/Pet.java
@@ -3,7 +3,14 @@ package com.sxtanna.mc.acolytes.data;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
 import com.sxtanna.mc.acolytes.backend.PetEntity;
+import com.sxtanna.mc.acolytes.backend.PlayerSkinAdapter;
+import com.sxtanna.mc.acolytes.data.attr.PetAttributes;
 
 public interface Pet
 {
@@ -16,5 +23,22 @@ public interface Pet
 	@Nullable <T> T select(@NotNull final PetAttribute<T> attribute);
 
 	@Nullable <T> T update(@NotNull final PetAttribute<T> attribute, @Nullable T value);
+
+
+	default @NotNull ItemStack createHeadItem(@NotNull final PlayerSkinAdapter adapter)
+	{
+		final ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+
+		final String skin = select(PetAttributes.SKIN);
+		if (skin != null)
+		{
+			final ItemMeta meta = head.getItemMeta();
+			adapter.updateSkullMeta(((SkullMeta) meta), skin);
+
+			head.setItemMeta(meta);
+		}
+
+		return head;
+	}
 
 }

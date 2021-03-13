b3f462107baa2fc3293a1e07a5c924f6a206ad63
Sxtanna <ranald.taylor@gmail.com>
2021-03-13 11:47:13 -0500

    added skin pet attribute

--- src/main/java/com/sxtanna/mc/acolytes/data/attr/PetAttributes.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/attr/PetAttributes.java
@@ -9,6 +9,7 @@ public enum PetAttributes
 
 	public static final PetAttribute<String> UUID = PetAttribute.make(String.class, "uuid", "", false);
 	public static final PetAttribute<String> NAME = PetAttribute.make(String.class, "name", "");
+	public static final PetAttribute<String> SKIN = PetAttribute.make(String.class, "skin", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTAyOGUzYzZmNDM1MGIzZTQyNGIzNzJmM2M5NDZmNTdiNzgzYjlhYTBlNGUyOTg5MWZmNTc4NDQxZWYwMTMwOCJ9fX0=");
 
 
 }

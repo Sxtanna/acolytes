2045dd1f4c6e1a4786566f62d5ad9193adaaff59
Sxtanna <ranald.taylor@gmail.com>
2021-03-13 11:50:39 -0500

    added pet object codec

---
+++ src/main/java/com/sxtanna/mc/acolytes/file/impl/ObjectCodecPet.java
@@ -0,0 +1,62 @@
+package com.sxtanna.mc.acolytes.file.impl;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.bukkit.configuration.ConfigurationSection;
+
+import com.sxtanna.mc.acolytes.data.Pet;
+import com.sxtanna.mc.acolytes.data.PetAttribute;
+import com.sxtanna.mc.acolytes.data.attr.PetAttributes;
+import com.sxtanna.mc.acolytes.data.impl.PetImpl;
+import com.sxtanna.mc.acolytes.file.ObjectCodec;
+
+import java.util.Locale;
+
+public final class ObjectCodecPet implements ObjectCodec<Pet>
+{
+
+	@NotNull
+	public static final ObjectCodec<Pet> INSTANCE = new ObjectCodecPet();
+
+
+	@Override
+	public @NotNull Pet pull(@NotNull final ConfigurationSection yaml)
+	{
+		final Pet pet = new PetImpl();
+
+		for (final String name : yaml.getKeys(false))
+		{
+			switch (name.toLowerCase(Locale.ROOT))
+			{
+				case "uuid":
+					pet.update(PetAttributes.UUID, PetAttributes.UUID.getType().cast(yaml.get(name)));
+					break;
+				case "name":
+					pet.update(PetAttributes.NAME, PetAttributes.NAME.getType().cast(yaml.get(name)));
+					break;
+				case "skin":
+					pet.update(PetAttributes.SKIN, PetAttributes.SKIN.getType().cast(yaml.get(name)));
+					break;
+				default:
+					throw new IllegalStateException(String.format("unknown attribute in pet: %s", name));
+			}
+		}
+
+		return pet;
+	}
+
+	@Override
+	public void push(@NotNull final ConfigurationSection yaml, @NotNull final Pet value)
+	{
+		for (final PetAttribute<?> attr : PetAttributes.ATTRIBUTES.values())
+		{
+			final Object data = value.select(attr);
+
+			if (data != null)
+			{
+				yaml.set(attr.getName(), data);
+			}
+		}
+	}
+
+}

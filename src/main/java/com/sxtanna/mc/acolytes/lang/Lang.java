6fa9540270156c1877cdc069c203d7e4841a7a3f
Sxtanna <ranald.taylor@gmail.com>
2021-03-14 15:46:11 -0400

    added lang enum

---
+++ src/main/java/com/sxtanna/mc/acolytes/lang/Lang.java
@@ -0,0 +1,26 @@
+package com.sxtanna.mc.acolytes.lang;
+
+import org.jetbrains.annotations.NotNull;
+
+import co.aikar.locales.MessageKey;
+import co.aikar.locales.MessageKeyProvider;
+
+import java.util.Locale;
+
+public enum Lang implements MessageKeyProvider
+{
+
+	CMDS__USERS__NAME_UPDATE,
+	CMDS__ADMIN__NAME_UPDATE__SENDER,
+	CMDS__ADMIN__NAME_UPDATE__TARGET;
+
+
+	@NotNull
+	private final MessageKey messageKey = MessageKey.of(name().toLowerCase(Locale.ROOT).replace("__", ".").replace("_", "-"));
+
+	@Override
+	public final @NotNull MessageKey getMessageKey()
+	{
+		return this.messageKey;
+	}
+}

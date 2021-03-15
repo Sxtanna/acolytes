4f1702be6a59aefbb3797f80ae8a280014c20b2d
Sxtanna <ranald.taylor@gmail.com>
2021-03-14 21:40:06 -0400

    updated pet menu with button lang messages and summon/remove logic

--- src/main/java/com/sxtanna/mc/acolytes/menu/impl/MenuPets.java
+++ src/main/java/com/sxtanna/mc/acolytes/menu/impl/MenuPets.java
@@ -4,20 +4,24 @@ import org.jetbrains.annotations.NotNull;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import com.sxtanna.mc.acolytes.AcolytesPlugin;
 import com.sxtanna.mc.acolytes.conf.AcolytesConfig;
 import com.sxtanna.mc.acolytes.data.Pet;
 import com.sxtanna.mc.acolytes.data.attr.PetAttributes;
+import com.sxtanna.mc.acolytes.lang.Lang;
 import com.sxtanna.mc.acolytes.menu.Menu;
 import com.sxtanna.mc.acolytes.util.bukkit.Stacks;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.sxtanna.mc.acolytes.menu.Menu.Size.forCount;
 import static com.sxtanna.mc.acolytes.util.bukkit.Colors.colorize;
+import static java.util.Arrays.asList;
 
 public final class MenuPets extends Menu
 {
@@ -41,11 +45,9 @@ public final class MenuPets extends Menu
 	@Override
 	protected void make()
 	{
-		final Map<String, Pet> pets = plugin.getModule().getController().getLoadedPets();
-
 		final AtomicInteger next = new AtomicInteger();
 
-		for (final Map.Entry<String, Pet> entry : pets.entrySet())
+		for (final Entry<String, Pet> entry : plugin.getModule().getController().getLoadedPets().entrySet())
 		{
 			final int slot = next.getAndIncrement();
 			if (slot > getInventory().getSize())
@@ -53,21 +55,73 @@ public final class MenuPets extends Menu
 				break; // paginate?
 			}
 
+			final Pet loaded = entry.getValue();
+			final Pet target = plugin.getModule().getController().getByUuid(player, loaded.getUuid()).orElse(null);
 
-			final ItemStack item = Stacks.meta(entry.getValue().createHeadItem(plugin.getModule().getAdapter()), meta ->
+			final ItemStack item = Stacks.meta(loaded.createHeadItem(plugin.getModule().getAdapter()), meta ->
 			{
-				entry.getValue().select(PetAttributes.NAME).ifPresent(name -> {
-					Stacks.name(meta, name);
-				});
+				String custom = null;
+
+				if (target != null)
+				{
+					custom = target.select(PetAttributes.NAME).orElse(null);
+				}
+
+				if (custom == null)
+				{
+					custom = loaded.select(PetAttributes.NAME).orElse(null);
+				}
+
+				Stacks.name(meta,
+				            custom != null ? custom : loaded.getUuid());
+
+				final List<String> lore = new ArrayList<>(asList((target != null ?
+				                                                  plugin.lang(player, Lang.MENU__BUTTONS__HAS_GIVEN) :
+				                                                  plugin.lang(player, Lang.MENU__BUTTONS__NOT_GIVEN)).split("\n")));
+				if (target != null)
+				{
+					lore.addAll(asList((target.getEntity() != null ?
+					                    plugin.lang(player, Lang.MENU__BUTTONS__REMOVE) :
+					                    plugin.lang(player, Lang.MENU__BUTTONS__SUMMON)).split("\n")));
+				}
+
+				Stacks.lore(meta, lore);
 			});
 
 
 			slot(slot, item, event ->
 			{
-				// todo: check if they own this pet first
+				if (target == null)
+				{
+					return;
+				}
+
+
+				final Optional<Pet> active = plugin.getModule().getController().getActive(player);
+				if (active.isPresent())
+				{
+					plugin.getModule().getController().kill(player, active.get());
+
+					plugin.send(player,
+					            Lang.MENU__REMOVED,
+
+					            "{pet_name}",
+					            target.select(PetAttributes.NAME).orElse(target.select(PetAttributes.UUID).orElse("Pet")));
+
+					if (active.get().equals(loaded))
+					{
+						return; // only despawn
+					}
+				}
 
 				player.closeInventory();
-				plugin.getModule().getController().load(player, entry.getValue());
+				plugin.getModule().getController().load(player, target);
+
+				plugin.send(player,
+				            Lang.MENU__SPAWNED,
+
+				            "{pet_name}",
+				            target.select(PetAttributes.NAME).orElse(target.select(PetAttributes.UUID).orElse("Pet")));
 			});
 		}
 

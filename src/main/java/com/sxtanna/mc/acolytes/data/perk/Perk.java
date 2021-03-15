b6ae0c1a6615731c05981f7129250363d6be2f14
Sxtanna <ranald.taylor@gmail.com>
2021-03-15 15:55:44 -0400

    added effect group perk

--- src/main/java/com/sxtanna/mc/acolytes/data/perk/Perk.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/perk/Perk.java
@@ -6,6 +6,10 @@ import org.jetbrains.annotations.NotNull;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.potion.PotionEffect;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public abstract class Perk
 {
 
@@ -27,7 +31,6 @@ public abstract class Perk
 
 
 	public abstract void apply(@NotNull final LivingEntity target);
-
 
 
 	public static final class Effect extends Perk
@@ -56,6 +59,41 @@ public abstract class Perk
 		public void apply(@NotNull final LivingEntity target)
 		{
 			target.addPotionEffect(getEffect(), true);
+		}
+
+	}
+
+	public static final class EffectGroup extends Perk
+	{
+
+		@NotNull
+		private final List<PotionEffect> effects;
+
+
+		public EffectGroup(@NotNull final List<PotionEffect> effects)
+		{
+			super(effects.stream().map(effect -> String.format("%s %d", effect.getType().getName(), effect.getAmplifier() + 1)).collect(Collectors.joining("\n")));
+
+			this.effects = effects;
+		}
+
+		public EffectGroup(@NotNull final PotionEffect... effects)
+		{
+			this(Arrays.asList(effects));
+		}
+
+
+		@Contract(pure = true)
+		public @NotNull List<PotionEffect> getEffects()
+		{
+			return this.effects;
+		}
+
+
+		@Override
+		public void apply(@NotNull final LivingEntity target)
+		{
+			getEffects().forEach(effect -> target.addPotionEffect(effect, true));
 		}
 
 	}

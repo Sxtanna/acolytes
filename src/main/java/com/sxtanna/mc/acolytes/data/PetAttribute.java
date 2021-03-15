3597b31d7cfdcba11a28eddd491fb2dbd0388c88
Sxtanna <ranald.taylor@gmail.com>
2021-03-15 15:56:13 -0400

    updated pet attributes to use a builder

--- src/main/java/com/sxtanna/mc/acolytes/data/PetAttribute.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/PetAttribute.java
@@ -2,9 +2,15 @@ package com.sxtanna.mc.acolytes.data;
 
 import org.jetbrains.annotations.NotNull;
 
+import org.bukkit.configuration.ConfigurationSection;
+
+import com.sxtanna.mc.acolytes.file.ObjectCodec;
+import com.sxtanna.mc.acolytes.file.ObjectReader;
+import com.sxtanna.mc.acolytes.file.ObjectWriter;
+
 import java.util.function.Function;
 
-public interface PetAttribute<T>
+public interface PetAttribute<T> extends ObjectCodec<T>
 {
 
 	@NotNull T getDefaultAttr();
@@ -21,55 +27,131 @@ public interface PetAttribute<T>
 	}
 
 
-	static <T> PetAttribute<T> make(@NotNull final Class<T> type, @NotNull final String name, @NotNull final T defaultAttr)
+	static <T> PetAttribute.Builder<T> builder(@NotNull final Class<T> type, @NotNull final String name)
 	{
-		return make(type, name, defaultAttr, true, Function.identity());
+		return new Builder<>(name, type);
 	}
 
-	static <T> PetAttribute<T> make(@NotNull final Class<T> type, @NotNull final String name, @NotNull final T defaultAttr, @NotNull final Function<T, T> copy)
-	{
-		return make(type, name, defaultAttr, true, copy);
-	}
 
-	static <T> PetAttribute<T> make(@NotNull final Class<T> type, @NotNull final String name, @NotNull final T defaultAttr, final boolean mutable)
+	class Builder<T>
 	{
-		return make(type, name, defaultAttr, mutable, Function.identity());
-	}
 
-	static <T> PetAttribute<T> make(@NotNull final Class<T> type, @NotNull final String name, @NotNull final T defaultAttr, final boolean mutable, @NotNull final Function<T, T> copy)
-	{
-		return new PetAttribute<T>()
+		private final String name;
+
+		private final Class<T> type;
+
+		private T defaultAttr;
+
+		private boolean mutable;
+
+		private Function<T, T> copy;
+
+		private ObjectReader<T> reader;
+
+		private ObjectWriter<T> writer;
+
+
+		private Builder(final String name, final Class<T> type)
 		{
-			@Override
-			public @NotNull T getDefaultAttr()
-			{
-				return defaultAttr;
-			}
+			this.name = name;
+			this.type = type;
+			this.copy = Function.identity();
+		}
 
-			@Override
-			public @NotNull T getCopiedValue(@NotNull final T value)
-			{
-				return copy.apply(value);
-			}
 
-			@Override
-			public @NotNull String getName()
-			{
-				return name;
-			}
+		public Builder<T> defaultAttr(@NotNull final T defaultAttr)
+		{
+			this.defaultAttr = defaultAttr;
+			return this;
+		}
 
-			@Override
-			public @NotNull Class<T> getType()
-			{
-				return type;
-			}
+		public Builder<T> mutable(final boolean mutable)
+		{
+			this.mutable = mutable;
+			return this;
+		}
 
-			@Override
-			public boolean isMutable()
+		public Builder<T> copier(final Function<T, T> copy)
+		{
+			this.copy = copy;
+			return this;
+		}
+
+		public Builder<T> reader(final ObjectReader<T> reader)
+		{
+			this.reader = reader;
+			return this;
+		}
+
+		public Builder<T> writer(final ObjectWriter<T> writer)
+		{
+			this.writer = writer;
+			return this;
+		}
+
+
+		public PetAttribute<T> build()
+		{
+			return new PetAttribute<T>()
 			{
-				return mutable;
-			}
-		};
+				@Override
+				public @NotNull T getDefaultAttr()
+				{
+					return defaultAttr;
+				}
+
+				@Override
+				public @NotNull T getCopiedValue(@NotNull final T value)
+				{
+					return copy.apply(value);
+				}
+
+				@Override
+				public @NotNull String getName()
+				{
+					return name;
+				}
+
+				@Override
+				public @NotNull Class<T> getType()
+				{
+					return type;
+				}
+
+				@Override
+				public boolean isMutable()
+				{
+					return mutable;
+				}
+
+				@Override
+				public @NotNull T pull(final @NotNull ConfigurationSection yaml)
+				{
+					if (reader != null)
+					{
+						return reader.pull(yaml);
+					}
+					else
+					{
+						return getType().cast(yaml.get(getName()));
+					}
+				}
+
+				@Override
+				public void push(final @NotNull ConfigurationSection yaml, @NotNull final T value)
+				{
+					if (writer != null)
+					{
+						writer.push(yaml, value);
+					}
+					else
+					{
+						yaml.set(getName(), value);
+					}
+				}
+			};
+		}
+
 	}
 
 }
--- src/main/java/com/sxtanna/mc/acolytes/data/attr/PetAttributes.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/attr/PetAttributes.java
@@ -2,25 +2,103 @@ package com.sxtanna.mc.acolytes.data.attr;
 
 import org.jetbrains.annotations.Unmodifiable;
 
-import com.sxtanna.mc.acolytes.data.PetAttribute;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import com.sxtanna.mc.acolytes.data.PetAttribute;
+import com.sxtanna.mc.acolytes.data.perk.Perk;
+import com.sxtanna.mc.acolytes.file.impl.ObjectCodecPotionEffect;
+
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public enum PetAttributes
 {
 	;
 
 
-	public static final PetAttribute<String> UUID = PetAttribute.make(String.class, "uuid", "", false);
-	public static final PetAttribute<String> NAME = PetAttribute.make(String.class, "name", "");
-	public static final PetAttribute<String> SKIN = PetAttribute.make(String.class, "skin", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTAyOGUzYzZmNDM1MGIzZTQyNGIzNzJmM2M5NDZmNTdiNzgzYjlhYTBlNGUyOTg5MWZmNTc4NDQxZWYwMTMwOCJ9fX0=");
+	public static final PetAttribute<String> UUID = PetAttribute.builder(String.class, "uuid")
+	                                                            .defaultAttr("")
+	                                                            .mutable(false)
+	                                                            .build();
+
+
+	public static final PetAttribute<String> NAME = PetAttribute.builder(String.class, "name")
+	                                                            .defaultAttr("")
+	                                                            .build();
+
+
+	public static final PetAttribute<String> SKIN = PetAttribute.builder(String.class, "skin")
+	                                                            .defaultAttr("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTAyOGUzYzZmNDM1MGIzZTQyNGIzNzJmM2M5NDZmNTdiNzgzYjlhYTBlNGUyOTg5MWZmNTc4NDQxZWYwMTMwOCJ9fX0=")
+	                                                            .build();
+
+
+	public static final PetAttribute<Perk.Effect> PERK_EFFECT = PetAttribute.builder(Perk.Effect.class, "perk_effect")
+	                                                                        .defaultAttr(new Perk.Effect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1, false, false)))
+	                                                                        .copier((perk) -> new Perk.Effect(new PotionEffect(perk.getEffect().getType(),
+	                                                                                                                           perk.getEffect().getDuration(),
+	                                                                                                                           perk.getEffect().getAmplifier(),
+	                                                                                                                           perk.getEffect().isAmbient(),
+	                                                                                                                           perk.getEffect().hasParticles())))
+	                                                                        .writer((yaml, value) -> {
+		                                                                        ObjectCodecPotionEffect.INSTANCE.push(yaml.createSection("perk_effect"), value.getEffect());
+	                                                                        })
+	                                                                        .reader(yaml -> {
+		                                                                        return new Perk.Effect(ObjectCodecPotionEffect.INSTANCE.pull(yaml.getConfigurationSection("perk_effect")));
+	                                                                        })
+	                                                                        .build();
+
+	public static final PetAttribute<Perk.EffectGroup> PERK_EFFECT_GROUP = PetAttribute.builder(Perk.EffectGroup.class, "perk_effect_group")
+	                                                                                   .defaultAttr(new Perk.EffectGroup(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1, false, false),
+	                                                                                                                     new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 1, false, false)))
+	                                                                                   .copier((perk) -> {
+		                                                                                   final List<PotionEffect> effects = new ArrayList<>();
+
+		                                                                                   for (final PotionEffect effect : perk.getEffects())
+		                                                                                   {
+			                                                                                   effects.add(new PotionEffect(effect.getType(),
+			                                                                                                                effect.getDuration(),
+			                                                                                                                effect.getAmplifier(),
+			                                                                                                                effect.isAmbient(),
+			                                                                                                                effect.hasParticles()));
+		                                                                                   }
+
+		                                                                                   return new Perk.EffectGroup(effects);
+	                                                                                   })
+	                                                                                   .writer((yaml, value) -> {
+		                                                                                   final ConfigurationSection section = yaml.createSection("perk_effect_group");
+
+		                                                                                   for (int i = 0; i < value.getEffects().size(); i++)
+		                                                                                   {
+			                                                                                   ObjectCodecPotionEffect.INSTANCE.push(section.createSection(String.valueOf(i)), value.getEffects().get(i));
+		                                                                                   }
+	                                                                                   })
+	                                                                                   .reader(yaml -> {
+
+		                                                                                   final ConfigurationSection section = yaml.getConfigurationSection("perk_effect_group");
+
+		                                                                                   final List<PotionEffect> effects = new ArrayList<>();
+
+		                                                                                   for (final String key : section.getKeys(false))
+		                                                                                   {
+			                                                                                   effects.add(ObjectCodecPotionEffect.INSTANCE.pull(section.getConfigurationSection(key)));
+		                                                                                   }
+
+		                                                                                   return new Perk.EffectGroup(effects);
+	                                                                                   })
+	                                                                                   .build();
 
 
 	@Unmodifiable
-	public static final Map<String, PetAttribute<?>> ATTRIBUTES = Collections.unmodifiableMap(Stream.of(UUID, NAME, SKIN).collect(Collectors.toMap(PetAttribute::getName, Function.identity())));
+	public static final Map<String, PetAttribute<?>> ATTRIBUTES = Collections.unmodifiableMap(Stream.of(UUID, NAME, SKIN, PERK_EFFECT, PERK_EFFECT_GROUP)
+	                                                                                                .collect(toMap(PetAttribute::getName,
+	                                                                                                               Function.identity())));
 
 }

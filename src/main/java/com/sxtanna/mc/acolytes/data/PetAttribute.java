1b3d42ad826351e67d475c97233007709d45e834
Sxtanna <ranald.taylor@gmail.com>
2021-03-17 12:01:06 -0400

    updated pet attribute builder with proper nullability and contracts

--- src/main/java/com/sxtanna/mc/acolytes/data/PetAttribute.java
+++ src/main/java/com/sxtanna/mc/acolytes/data/PetAttribute.java
@@ -1,6 +1,8 @@
 package com.sxtanna.mc.acolytes.data;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import org.bukkit.configuration.ConfigurationSection;
 
@@ -9,6 +11,8 @@ import com.sxtanna.mc.acolytes.file.ObjectReader;
 import com.sxtanna.mc.acolytes.file.ObjectWriter;
 
 import java.util.function.Function;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public interface PetAttribute<T> extends ObjectCodec<T>
 {
@@ -24,31 +28,39 @@ public interface PetAttribute<T> extends ObjectCodec<T>
 	boolean isMutable();
 
 
-	static <T> PetAttribute.Builder<T> builder(@NotNull final Class<T> type, @NotNull final String name)
+	@Contract(value = "_, _ -> new", pure = true)
+	static <T> @NotNull Builder<T> builder(@NotNull final Class<T> type, @NotNull final String name)
 	{
 		return new Builder<>(name, type);
 	}
 
 
-	class Builder<T>
+	final class Builder<T>
 	{
 
+		@NotNull
 		private final String name;
 
+		@NotNull
 		private final Class<T> type;
 
+		@Nullable
 		private T defaultAttr;
 
 		private boolean mutable = true;
 
+		@NotNull
 		private Function<T, T> copy;
 
+		@Nullable
 		private ObjectReader<T> reader;
 
+		@Nullable
 		private ObjectWriter<T> writer;
 
 
-		private Builder(final String name, final Class<T> type)
+		@Contract(pure = true)
+		private Builder(final @NotNull String name, final @NotNull Class<T> type)
 		{
 			this.name = name;
 			this.type = type;
@@ -56,39 +68,47 @@ public interface PetAttribute<T> extends ObjectCodec<T>
 		}
 
 
+		@Contract(value = "_ -> this", mutates = "this")
 		public Builder<T> defaultAttr(@NotNull final T defaultAttr)
 		{
 			this.defaultAttr = defaultAttr;
 			return this;
 		}
 
+		@Contract(value = "_ -> this", mutates = "this")
 		public Builder<T> mutable(final boolean mutable)
 		{
 			this.mutable = mutable;
 			return this;
 		}
 
-		public Builder<T> copier(final Function<T, T> copy)
+		@Contract(value = "_ -> this", mutates = "this")
+		public Builder<T> copier(@NotNull final Function<T, T> copy)
 		{
 			this.copy = copy;
 			return this;
 		}
 
-		public Builder<T> reader(final ObjectReader<T> reader)
+		@Contract(value = "_ -> this", mutates = "this")
+		public Builder<T> reader(@NotNull final ObjectReader<T> reader)
 		{
 			this.reader = reader;
 			return this;
 		}
 
-		public Builder<T> writer(final ObjectWriter<T> writer)
+		@Contract(value = "_ -> this", mutates = "this")
+		public Builder<T> writer(@NotNull final ObjectWriter<T> writer)
 		{
 			this.writer = writer;
 			return this;
 		}
 
 
-		public PetAttribute<T> build()
+		@Contract(value = " -> new", pure = true)
+		public @NotNull PetAttribute<T> build()
 		{
+			checkNotNull(defaultAttr, "default attribute value must be provided!");
+
 			return new PetAttribute<T>()
 			{
 				@Override

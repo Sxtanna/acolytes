b06ca1e1a7a9add2fd8f51ed910568d14df9d7cb
Sxtanna <ranald.taylor@gmail.com>
2021-03-18 11:45:50 -0400

    added getters for meta values

--- src/main/java/com/sxtanna/mc/acolytes/util/bukkit/Stacks.java
+++ src/main/java/com/sxtanna/mc/acolytes/util/bukkit/Stacks.java
@@ -1,12 +1,15 @@
 package com.sxtanna.mc.acolytes.util.bukkit;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -67,6 +70,47 @@ public enum Stacks
 	public static void flag(@NotNull final ItemMeta meta, @NotNull final ItemFlag... flags)
 	{
 		meta.addItemFlags(flags);
+	}
+
+
+	public static void clean(@NotNull final ItemMeta meta)
+	{
+		name(meta, " ");
+		lore(meta, Collections.emptyList());
+		flag(meta, ItemFlag.values());
+	}
+
+
+	public static @Nullable String getName(@NotNull final ItemStack item)
+	{
+		if (!item.hasItemMeta())
+		{
+			return null;
+		}
+
+		final ItemMeta meta = item.getItemMeta();
+		if (!meta.hasDisplayName())
+		{
+			return null;
+		}
+
+		return meta.getDisplayName();
+	}
+
+	public static @NotNull List<String> geLore(@NotNull final ItemStack item)
+	{
+		if (!item.hasItemMeta())
+		{
+			return Collections.emptyList();
+		}
+
+		final ItemMeta meta = item.getItemMeta();
+		if (!meta.hasLore())
+		{
+			return Collections.emptyList();
+		}
+
+		return meta.getLore();
 	}
 
 }
